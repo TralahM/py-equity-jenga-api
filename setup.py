2b616e7e05eda75c0620e43e64892a38318304c4
TralahM <musyoki.brian@tralahtek.com>
2020-05-23 09:21:25 +0300

    populating pypi conf setup,manifest,pyproject

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+=======================================================================
+:AUTHOR:	 Tralah M Brian <briantralah@gmail.com>
+:TWITTER: 	 @TralahM <https://twitter.com/TralahM>
+:GITHUB: 	 <https://github.com/TralahM>
+:KAGGLE: 	 <https://kaggle.com/TralahM>
+:COPYRIGHT:  (c) 2020  TralahTek LLC.
+:LICENSE: 	 MIT, see LICENSE for more details.
+:CREATED: 	2020-01-17  06:19
+=======================================================================
+
+DESCRIPTION
+"""
+
+from setuptools import setup
+
+setup()
